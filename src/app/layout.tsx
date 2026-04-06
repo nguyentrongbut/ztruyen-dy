282994ba9d8993bf38a89f6979ed59e40a324cb4
nguyentrongbut <cloly031128@gmail.com>
2026-04-06 10:53:56 +0700

    fix(ga4): change position script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -150,20 +150,19 @@ export default function RootLayout({
                         <ToasterCustom />
                     </NprogressWrapper>
                 </ThemeProvider>
+                <Script
+                    src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+                    `}
+                </Script>
             </body>
-            <Script
-                src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
-                strategy="afterInteractive"
-            />
-
-            <Script id="google-analytics" strategy="afterInteractive">
-                {`
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-    gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
-  `}
-            </Script>
         </html>
     );
 }
